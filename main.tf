092b9d3a38669f91a6a0441647ef279850e7c39b
endrieb99 <bida.endri1@gmail.com>
2022-05-26 15:54:18 +0200

    declare variableS

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  access_key = "ASIAWQ66XTQ3UZ7AWB6G"
-  secret_key = "PWnMBHroprk9VoLNRKZs9aL4A6EsZ8cQsqW5+p97"
-  token = "FwoGZXIvYXdzEM3//////////wEaDLztbbPEe0rBWX5/MCLJAd5EGu9GMK+m5NJ4UmsKNdu2aMeVeuGdUtsKrw8V/A3k0elEKAI+w8Iq9MaYjeuVjTg8eeAIziQZoVxGUKlnPuDgkosgT4woir4uxT90R3tyL25oVuC6fsVzjcvDMePeujVE1l7jOkWdrdp3+NzE2BCszN8N/nY2V4McbPUMCAuRUBwDNesLRKLddDIiNZHVguYdn7tVpqlN+yKVXfT2gtqma03ef4wDpivgiX28O6tMi7akLTS/Y673b7SeWd5W1HzCd66xWr8c+Sj82L2UBjIti/QsPh/fkd4uQwkn4awO+pFktUD/7CXU2ij6NW7O9I5qj49vxPj3wOp9z6rX"
-  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  token = var.aws_session_token
+  region = var.aws_region
 }
 
 resource "aws_instance" "app_server" {
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key" {
+ type = string
+ default = "ASIAWQ66XTQ3UZ7AWB6G"
+}
+
+variable "aws_secret_key" {
+  type = string
+  default = "PWnMBHroprk9VoLNRKZs9aL4A6EsZ8cQsqW5+p97"
+}
+
+variable "aws_session_token" {
+  type = string
+  default = "FwoGZXIvYXdzEM3//////////wEaDLztbbPEe0rBWX5/MCLJAd5EGu9GMK+m5NJ4UmsKNdu2aMeVeuGdUtsKrw8V/A3k0elEKAI+w8Iq9MaYjeuVjTg8eeAIziQZoVxGUKlnPuDgkosgT4woir4uxT90R3tyL25oVuC6fsVzjcvDMePeujVE1l7jOkWdrdp3+NzE2BCszN8N/nY2V4McbPUMCAuRUBwDNesLRKLddDIiNZHVguYdn7tVpqlN+yKVXfT2gtqma03ef4wDpivgiX28O6tMi7akLTS/Y673b7SeWd5W1HzCd66xWr8c+Sj82L2UBjIti/QsPh/fkd4uQwkn4awO+pFktUD/7CXU2ij6NW7O9I5qj49vxPj3wOp9z6rX"
+}
+
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
